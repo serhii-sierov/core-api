c983cd436ecd0cd5bcd97dfa85dedbf61ff6540f
serhii-sierov <serhii.sierov@sysgears.com>
2024-10-07 22:13:45 +0300

    Add Husky

---
+++ .husky/check-migrations.sh
@@ -0,0 +1,8 @@
+# cd ../lotic-configs
+docker compose up boilerplate -d
+result=$(docker compose exec boilerplate yarn migration:generate test 2>/dev/null)
+if [[ $result != *"No changes in database schema were found"* ]];
+then 
+  echo "Migrations pending: $result" >&2 
+  exit 1 
+fi
