ab6b7fc915c522d887298d00a6bf481f87644c50
serhii-sierov <serhii.sierov@sysgears.com>
2024-10-08 01:37:11 +0300

    Move envValidationErrorFunction

---
+++ src/utils/dotenv/env-validation-error-function.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-unresolved -- path alias
+import { DOCUMENTATION_URL } from '@constants';
+import Joi from 'joi';
+
+export const envValidationErrorFunction = (errors: Joi.ErrorReport[]) =>
+  new Error(
+    [
+      ...errors,
+      '\x1b[1m\x1b[31mMake sure the environment variables are set correctly\x1b[0m',
+      `\x1b[1m\x1b[31m${DOCUMENTATION_URL}\x1b[0m`,
+    ].join('\n '),
+  );
