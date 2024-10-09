c415103e8453fffb2d52788f1e8a41df60d79651
serhii-sierov <serhii.sierov@sysgears.com>
2024-10-09 20:49:06 +0300

    Add generate-typings script

---
+++ src/scripts/generate-typings.ts
@@ -0,0 +1,10 @@
+import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
+import { join } from 'path';
+
+const definitionsFactory = new GraphQLDefinitionsFactory();
+
+void definitionsFactory.generate({
+  typePaths: ['./src/**/*.graphql'],
+  path: join(process.cwd(), 'src/graphql.ts'),
+  outputAs: 'interface',
+});
