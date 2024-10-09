e7e63fd485094c1f6f6a9eeaf3b56379208570ef
serhii-sierov <serhii.sierov@sysgears.com>
2024-10-09 23:56:59 +0300

    Add User module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from './modules/shared/shared.module';
 import { HealthModule } from 'modules/health';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [SharedModule, HealthModule],
+  imports: [SharedModule, HealthModule, UserModule],
   providers: [],
 })
 export class AppModule {}
---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+}
