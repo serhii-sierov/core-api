55c0b1c4482ee755a9451376840bb037f380a272
serhii-sierov <serhii.sierov@sysgears.com>
2024-10-13 23:01:56 +0300

    Refactor EnvironmentVariables type

--- src/types/env.ts
+++ src/types/env.ts
@@ -1,18 +1,18 @@
 import { Environments } from '@constants';
 
-import { Config } from 'config';
+import { Config, JwtConfig } from 'config';
 
 export type EnvironmentVariables = Config & {
   NODE_ENV: Environments;
   GIT_COMMIT: string;
-  ENVIRONMENT: Environments;
   PORT: number;
+
+  jwt: JwtConfig;
 
   DATABASE_URL: string;
   TEST_DATABASE_URL: string;
   DATABASE_LOGGING: boolean;
   DATABASE_SSL: boolean;
-  IS_LOCAL: boolean;
 
   npm_package_version: string; //predefined by node from package.json
 
