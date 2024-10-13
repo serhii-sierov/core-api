b77ab918cf5cb2c1bc88624490544506d7fb6487
serhii-sierov <serhii.sierov@sysgears.com>
2024-10-13 23:06:48 +0300

    Add global guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 
+import { AccessTokenGuard } from 'modules/auth/guards/accessToken.guard';
 import { HealthModule } from 'modules/health';
 
+import { AuthModule } from './modules/auth/auth.module';
 import { SharedModule } from './modules/shared/shared.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [SharedModule, HealthModule, UserModule],
-  providers: [],
+  imports: [SharedModule, HealthModule, UserModule, AuthModule],
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: AccessTokenGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,3 @@
 export * from './env';
+
+export * from './app-cookie';
