9abc9801b4bd95f2df21cca67c7df714572049c0
serhii-sierov <serhii.sierov@sysgears.com>
2024-10-13 23:07:05 +0300

    Update graphql.ts

--- src/graphql.ts
+++ src/graphql.ts
@@ -8,6 +8,31 @@
 /* tslint:disable */
 /* eslint-disable */
 
+export interface SignInInput {
+    email: string;
+    password: string;
+    rememberMe?: Nullable<boolean>;
+    device?: Nullable<string>;
+}
+
+export interface SignUpInput {
+    email: string;
+    password: string;
+    device?: Nullable<string>;
+}
+
+export interface SignOutInput {
+    isAllDevices?: Nullable<boolean>;
+}
+
+export interface RefreshTokenInput {
+    device?: Nullable<string>;
+}
+
+export interface ResetPasswordInput {
+    email: string;
+}
+
 export interface DefaultResponseInterface {
     status: string;
     message?: Nullable<string>;
@@ -15,10 +40,16 @@ export interface DefaultResponseInterface {
 
 export interface IQuery {
     _empty(): Nullable<string> | Promise<Nullable<string>>;
+    user(): Nullable<User> | Promise<Nullable<User>>;
 }
 
 export interface IMutation {
     _empty(): Nullable<string> | Promise<Nullable<string>>;
+    signUp(input: SignUpInput): Nullable<SignUpResponse> | Promise<Nullable<SignUpResponse>>;
+    signIn(input: SignInInput): Nullable<SignInResponse> | Promise<Nullable<SignInResponse>>;
+    signOut(input?: Nullable<SignOutInput>): Nullable<boolean> | Promise<Nullable<boolean>>;
+    refreshToken(input?: Nullable<RefreshTokenInput>): Nullable<boolean> | Promise<Nullable<boolean>>;
+    resetPassword(input: ResetPasswordInput): Nullable<DefaultResponse> | Promise<Nullable<DefaultResponse>>;
 }
 
 export interface ISubscription {
@@ -28,6 +59,19 @@ export interface ISubscription {
 export interface DefaultResponse extends DefaultResponseInterface {
     status: string;
     message?: Nullable<string>;
+}
+
+export interface SignInResponse {
+    user?: Nullable<User>;
+}
+
+export interface SignUpResponse {
+    user?: Nullable<User>;
+}
+
+export interface User {
+    id: number;
+    email: string;
 }
 
 export type JSON = any;
