9d1124efe09bacfe45fcbd9e4bb4c26ad9d2fff2
serhii-sierov <serhii.sierov@sysgears.com>
2024-10-14 21:47:07 +0300

    Fix configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,7 +35,7 @@ module.exports = {
   rules: {
     // General
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-floating-promises': 'warn',
